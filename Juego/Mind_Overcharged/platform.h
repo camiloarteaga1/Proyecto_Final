c14abd3cb777a2a063154625d7d5b021b43ab7fb
Daniel León <65270757+DanielLeonD@users.noreply.github.com>
2020-08-02 16:22:16 -0500

    Added platform class (rework)

--- Juego/Mind_Overcharged/platform.h
+++ Juego/Mind_Overcharged/platform.h
@@ -1,36 +1,21 @@
-/*#ifndef PLATFORM_H
+#ifndef PLATFORM_H
 #define PLATFORM_H
 
 #include <QGraphicsPixmapItem>
 #include <QObject>
 
-#include "player.h"
-
-/// Max size = 10*10 Px
 class Platform: public QGraphicsPixmapItem
 {
-    Q_OBJECT
 public:
-    Platform(QGraphicsItem * = nullptr);
-    Platform(float Pos_X, float Pos_Y, float Friction, float ConductivityResistance, float AmbientTemperature, QGraphicsItem * = nullptr);
-    float getFriction();
-    float getTemperature();
-    bool getStatus();
+    Platform(float Friction = 1, bool IsSolid = 1, QString FileName = "", QGraphicsItem * = nullptr);
 
-    void setStatus(bool);
-
-public slots:
-    void ConductionEffects();
-    void TemperatureChange();
+    double getFriction();
+    bool IsSolid();
 
 private:
-    float Friction;
-    const float TempBase;
-    float Temp;
-    float CondRes;
-    bool isActive;
+    const double Friction;
+    const bool Solid;
 
 };
 
 #endif // PLATFORM_H
-*/
