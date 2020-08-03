1974d146e4cbd5e54881ea8423106b8e3fa3940c
Daniel León <65270757+DanielLeonD@users.noreply.github.com>
2020-08-03 12:06:45 -0500

    Player rework

--- Juego/Mind_Overcharged/player.h
+++ Juego/Mind_Overcharged/player.h
@@ -8,10 +8,12 @@
 
 #define MAX_LIFES 3 /// Player's max lifes
 #define MAX_CHARGE 770 /// Player's max head charge
+#define MAX_SEPARATE_TIME 10.00 /// Time head can be separated from the body in sec
 #define PI 3.1415 /// Pi constant declaration
-#define G 0.00 //0.0988 /// Gravity
+#define G 0.0988 /// Gravity
 #define MAX_X_SPEED 3.00 /// Max horizontal player speed
-#define Vt(Mass) pow((2 * Mass * G) / (1.17 * 0.06 * 0.4), 0.5) /// Terminal speed formula
+#define Vt(Mass) pow((2 * Mass * G) / (1.17 * 0.06 * 0.6), 0.5) /// Terminal speed formula
+
 
 class Player: public QObject, public QGraphicsPixmapItem
 {
@@ -21,77 +23,86 @@ public:
     Player(short HeadMass, short BodyMass, short ID, QGraphicsItem * = nullptr);
 
     void ThrowObj();
-
-    //template<typename T>
-    //void ObjInteraction(T &Object);
+    void PlayerDie(); /// Do
 
     ///**************************  Getters  **************************///
     /// Body
-    float get_BX();
-    float get_BY();
     float getAccel_BX();
     float getAccel_BY();
     float getSpeed_BX();
     float getSpeed_BY();
+    double getBodyGroundFriction();
+    bool getOnPlatform();
 
     /// Head
-    float get_HX();
-    float get_HY();
     float getAccel_HX();
     float getAccel_HY();
     float getSpeed_HX();
     float getSpeed_HY();
+    double getHeadGroundFriction();
+    bool getHeadOnPlatform();
 
     /// Player data
     short getLifes();
-    short getID();
+    short getID(); /// Revizar
     short getCharge();
     bool getHeadStatus();
+    short getSeparateTime();
     Qt::Key getKeyPressing();
     double getAirResistance();
-    double getGroundFriction();
+    short getInmunityTime();
 
     ///**************************  Setters  **************************///
     /// Body
-    void set_BX(float Pos_X);
-    void set_BY(float Pos_Y);
     void setAccel_BX(float Acceleration_X);
     void setAccel_BY(float Acceleration_Y);
     void setSpeed_BX(float Speed_X);
     void setSpeed_BY(float Speed_Y);
+    void setBodyGroundFriction(double GroundFriction);
+    void setOnPlatform(bool Status);
 
     /// Head
-    void set_HX(float Pos_X);
-    void set_HY(float Pos_Y);
     void setAccel_HX(float Acceleration_X);
     void setAccel_HY(float Acceleration_Y);
     void setSpeed_HX(float Speed_X);
     void setSpeed_HY(float Speed_Y);
+    void setHeadGroundFriction(double GroundFriction);
+    void setHeadOnPlatform(bool Status);
 
     /// Player data
     void setLifes(short LifesCount);
     void setCharge(short Charge);
     void setHeadStatus(bool HaveIt);
+    void setSeparateTime(short TimeSec);
     void setKeyPressing(Qt::Key);
     void setAirResistance(double AirResistance);
-    void setGroundFriction(double GroundFriction);
+    void setInmunityTime(short TimeSec);
+
 
     ///************************** Timers controlers  **************************///
     /// Move Timer
-    void MoveStart(int TimeMls);
+    void MoveStart(int TimeMls = 10);
     void MoveStop();
 
     /// Throw Timer
-    void ThrowStart(int TimeMls);
+    void ThrowStart(int TimeMls = 10);
     void ThrowStop();
 
     /// Discharge Timer
-    void DischargeStart(int TimeMls);
+    void DischargeStart(int TimeMls = 142);
     void DischargeStop();
 
     /// Sprites Timer
-    void SpritesStart(int TimeMls);
+    void SpritesStart(int TimeMls = 500);
     void SpritesStop();
+
+    /// 10 seconds Timer
+    void SeparateStart(int TimeMls = 1000);
+    void SeparateStop();
+
+    /// Inmunity Timer
+    void InmunityStart(int TimeMls = 1000);
+    void InmunityStop();
 
 private slots:
     /// Interactions
@@ -99,6 +110,8 @@ private slots:
     void MoveHead();
     void DischargeHead();
     void PickUpHead();
+    void SeparateHead();
+    void InmunityTime();
 
 public slots:
     /// Sprites
@@ -111,15 +124,16 @@ public:
 
 private:
 
-    //QGraphicsPixmapItem Body;
-
     bool HeadStatus;
-    Qt::Key CurrentKey;
+    bool OnPlatform;
+    bool HeadOnPlatform;
 
     QTimer *MoveTimer;
     QTimer *ThrowTimer;
     QTimer *DischargeHeadTimer;
     QTimer *SpritesTimer;
+    QTimer *SeparateHeadTimer;
+    QTimer *InmunityTimer;
 
     QPointF BAccel, BSpeed; /// Body
     QPointF HAccel, HSpeed; /// Head
@@ -128,10 +142,26 @@ private:
     const short ID;
     short HeadCharge;
     short P_Lifes;
+    short SepTime;
+    short Inmunity;
 
-    double AirRes, GroundFr;
-
+    double AirRes, BGroundFr, HGroundFr;
+    Qt::Key CurrentKey;
 
 };
+
+
+///***********************  Undefine Macros  ***********************///
+/*#ifdef PLAYER_MACROS
+
+#undef MAX_LIFES
+#undef MAX_CHARGE
+#undef MAX_SEPARATE_TIME
+#undef PI
+#undef G
+#undef MAX_X_SPEED
+#undef Vt
+
+#endif*/
 
 #endif // PLAYER_H
